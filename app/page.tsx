ff2bac154947fbf964e6c85273950ac272782e99
Justin Fowler <justinfowler1996@gmail.com>
2025-06-29 10:59:12 -0500

    better startup script

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { SignOut } from "./components/SignOut";
 export default async function Home() {
   const session = await auth();
   console.log(session);
-  const otherUsers = await fetch("/api/users").then((res) => res.json());
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -18,9 +17,6 @@ export default async function Home() {
         ) : (
           <SignIn />
         )}
-        {otherUsers.map((user) => (
-          <div key={user.id}>{user.email}</div>
-        ))}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
