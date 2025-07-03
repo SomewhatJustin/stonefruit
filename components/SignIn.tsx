39d658c6f9e8fb7fd991df4e4c39f00bc982a8b6
Justin Fowler <justinfowler1996@gmail.com>
2025-07-03 18:47:57 -0500

    is this updating

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,12 +1,12 @@
-import { signIn } from "@/auth"
+import { signIn } from "@/auth";
 
 export function SignIn() {
   return (
     <form
       action={async (formData: FormData) => {
-        "use server"
+        "use server";
         // Email provider id (matches auth.ts configuration)
-        await signIn("mailgun", formData)
+        await signIn("mailgun", formData);
       }}
       className="flex flex-col gap-2"
     >
@@ -21,8 +21,8 @@ export function SignIn() {
         type="submit"
         className="bg-black text-white px-4 py-2 rounded hover:bg-neutral-800"
       >
-        Sign in with Mailgun
+        Sign in with Email
       </button>
     </form>
-  )
-} 
+  );
+}
