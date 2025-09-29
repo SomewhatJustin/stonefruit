23ba3e879e708477d714925509e7e51c68b837d7
Justin Fowler <justinfowler1996@gmail.com>
2025-09-29 18:25:21 -0500

    init db

--- server/server.js
+++ server/server.js
@@ -1,31 +1,37 @@
-'use strict'
+"use strict";
 
-import Fastify from 'fastify'
-import { PrismaClient } from './generated/prisma/index.js'
+import Fastify from "fastify";
+import { PrismaClient } from "./generated/prisma/index.js";
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 // Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
 
-})
+fastify.put("/new-user", function (request, reply) {
+  const new_user = prisma.user.create({
+    data: { fname: request.fname },
+  });
+});
 
-fastify.post('/', function (request, reply) {
-  const message = prisma.Message.create({ data: { content: request.body.content } })
-  return message
-})
+fastify.post("/", function (request, reply) {
+  const message = prisma.message.create({
+    data: { content: request.body.content },
+  });
+  return message;
+});
 
 // Run the server!
-fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
   // Server is now listening on ${address}
-})
+});
